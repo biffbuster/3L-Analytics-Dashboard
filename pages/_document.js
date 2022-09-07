46803869f85e21594f3fa5224da1488f55541f8e
Biff Buster <biffbuster0@gmail.com>
2022-09-07 13:26:11 -0500

    font dependencies

--- pages/_document.js
+++ pages/_document.js
@@ -25,6 +25,8 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
           />
+          <link href="/fonts/font_style.css" rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
